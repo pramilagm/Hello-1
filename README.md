d512f9836b0ebab1430d828ff2972052cca821ff
Karina LaMarr <buzwebz@gmail.com>
2020-01-13 22:05:47 -0800

    test

--- README.md
+++ README.md
@@ -5,3 +5,46 @@ Login and Email -- (ZacK , Pramila, Karina)
 
 Socket.io 
 
+Deployment
+
+Mongo DB
+
+Mongoose 
+Login with Facebook, Google, Email
+
+Access Control
+
+Front-End 
+
+Deployment AWS  S3 Storage
+
+#Privacy
+    User Acc
+    Chat
+
+UX Design
+
+Send Smile & Hearts
+
+Notification (alert)
+
+Chat History
+
+Accepting Payments
+
+Share Post
+
+More UX Design
+
+Domain Name
+
+Frind Request
+
+Upload Pictures
+
+Socket.io
+
+HTTPs Certificate 
+
+
+
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const app = express();
-const port = 3000;
+// environment variable for port
+const port = process.env.PORT || 3000;
 //set up view engine 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', {
+        title: 'Home'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {
+        title: 'About'
+    });
 
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact');
+    res.render('contact', {
+        title: 'Contact'
+    });
 });
 
 app.listen(port, () => {
