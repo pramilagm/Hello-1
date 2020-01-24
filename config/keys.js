341b365810c25c3311a4966adcce49bb148ef869
Karina LaMarr <buzwebz@gmail.com>
2020-01-23 22:21:04 -0800

    new HELLO update

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MongoDB: 'mongodb+srv://onlinedatingapp:Hello88@hellodatingapp-0hxpf.mongodb.net/test?retryWrites=true&w=majority',
+    FacebookAppID: '622502495164064',
+    FacebookAppSecret: '9cc9c4e6ab223c8abd49e8c8a47f0f15'
+
+};
