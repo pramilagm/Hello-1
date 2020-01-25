28ad9c5f071c7dd3338164595d7ff74e2dfb450a
pramilagm <pmiris.pramila@gmail.com>
2020-01-25 13:51:20 -0800

    Adding Angular

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { DirectMessagingComponent } from "./direct-messaging/direct-messaging.component";
+import { MdNavComponent } from "./md-nav/md-nav.component";
+import { SplashComponent } from "./splash/splash.component";
 
-const routes: Routes = [{ path: "", component: ReadComponent }];
+const routes: Routes = [
+  { path: "messages", component: DirectMessagingComponent },
+  { path: "nav", component: MdNavComponent },
+  { path: "splash", component: SplashComponent },
+  // use a colon and parameter name to include a parameter in the url
+  // redirect to /alpha if there is nothing in the url
+  { path: "", pathMatch: "full", redirectTo: "/" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- public/src/app/md-nav/md-nav.component.ts
+++ public/src/app/md-nav/md-nav.component.ts
@@ -1,37 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-md-nav',
-  templateUrl: './md-nav.component.html',
-  styleUrls: ['./md-nav.component.css']
+  selector: "app-md-nav",
+  templateUrl: "./md-nav.component.html",
+  styleUrls: ["./md-nav.component.css"]
 })
 export class MdNavComponent implements OnInit {
+  sm_mobile: Boolean;
+  md_mobile: Boolean;
+  md_screen: Boolean;
+  lg_screen: Boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(next()) {
-    if (window.screen.width < 361) { 
-      this.sm-mobile = true;
-      next();
-    }
-    else-if (window.screen.width < 768) { 
-      this.md-mobile = true;
-      this.sm -mobile = false;
-      next();
-    }
-    else-if (window.screen.width < 1068) { 
-      this.md-screen = true;
-      this.md-mobile = false;
-      this.sm-mobile = false;
-      next();
-    }
-    else-if (window.screen.width > 1069) { 
-      this.lg-screen = true;
-      this.md-screen = false;
-      this.md-mobile = false;
-      this.sm-mobile = false;
-      next();
+  ngOnInit() {
+    if (window.screen.width < 361) {
+      this.sm_mobile = true;
+    } else if (window.screen.width < 768) {
+      this.md_mobile = true;
+      this.sm_mobile = false;
+    } else if (window.screen.width < 1068) {
+      this.md_screen = true;
+      this.md_mobile = false;
+      this.sm_mobile = false;
+    } else if (window.screen.width > 1069) {
+      this.lg_screen = true;
+      this.md_screen = false;
+      this.md_mobile = false;
+      this.sm_mobile = false;
     }
   }
-
 }
